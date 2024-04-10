4ff30765523c8011bffca6d7813bae257e2c067b
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-10 14:06:03 +0200

    feat: AppTitle

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script>
+  import AppTitle from './components/AppTitle.vue';
   export default {
     components: {
-      
+      AppTitle
     }
   }
 </script>
 
-<template></template>
+<template>
+  <main>
+    <AppTitle></AppTitle>
+  </main>
+</template>
 
-<style></style>
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,36 @@
+<script>
+    export default {
+        name: 'AppTitle',
+        data() {
+            return {
+                title: ''
+            }
+        },
+        methods: {
+            helloWorld(){
+                this.title = 'Hello World';
+            }
+          }
+    }
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+    <button @click="helloWorld">Saluta</button>
+</template>
+
+<style scope>
+    h1{
+        font-size: 4.375rem;
+    }
+    button {
+        width: 6.25rem;
+        height: 3.125rem;
+        background-color: lightblue;
+        border: none;
+        border-radius: 1.25rem;
+        font-size: 1.25rem;
+        cursor: pointer;
+        flex-shrink: 0;
+    }
+</style>
